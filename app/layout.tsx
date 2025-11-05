596fff00085f404ae4c21e79ded9b6ca2062773c
jerryavance <anguzuvj@gmail.com>
2025-11-05 16:44:39 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: 'Travel',
-  description: 'Travel UI/UX App for Camping',
+  title: 'SafariDrift',
+  description: 'Built for adventure. Driven by passion.',
 }
 
 export default function RootLayout({
